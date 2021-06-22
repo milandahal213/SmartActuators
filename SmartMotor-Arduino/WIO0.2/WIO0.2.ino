8dc7958e10efdb50bbac39245e3fdbbe4c0cd28c
milandahal213 <milandahal213@gmail.com>
2021-06-22 14:42:00 -0400

    Update WIO0.2.ino

--- SmartMotor-Arduino/WIO0.2/WIO0.2.ino
+++ SmartMotor-Arduino/WIO0.2/WIO0.2.ino
@@ -1,5 +1,6 @@
 #include "Servo.h"
 #include"TFT_eSPI.h"
+#include<arduino.h>
 
 #define LCD_BACKLIGHT (72Ul) // Control Pin of LCD
 Servo myservo1;
@@ -22,16 +23,25 @@ int training[10][2];
 int inc=1;
 int count=0;
 int pos = 0;
+int old_pos=0;
 int brightness = 0;
 int i=0;
 int dist=0;
 int mini=0;
 int DCount=0;
-String state="Training";
+String state="train";
 bool state_flag= true ;
 
-char * menuNames[]={"Start Application","How to ..."};
+//training mode
+char  howMany[16];
+char howManyNow[16];
+char * menuNames[]={"Start Application","Read Information"};
 
+
+
+//display motor values
+float x_motor=0.0;
+float y_motor=0.0;
 TFT_eSPI tft;
 #define LCD_BACKLIGHT (72Ul) // Control Pin of LCD
 
@@ -41,8 +51,10 @@ void setup() {
   tft.begin();
   tft.setRotation(3);
   Serial.begin(115200);
+  
   tft.fillScreen(green);
   myservo1.attach(D1);
+  
   pinMode(WIO_5S_UP,INPUT);
   pinMode(WIO_5S_DOWN,INPUT);
     
@@ -59,12 +71,13 @@ void setup() {
 
   tft.setTextColor(TFT_BLACK, TFT_BLACK);
 
-
+  drawGraphics(0);
 }
 
 void loop() {
   selectd=menu();
   if( selectd==0){
+    wiodisplay();
     smartMotor();
   }
   else if(selectd==1){
@@ -80,6 +93,10 @@ void loop() {
 
 
 int menu(){
+    makeButton(100,140,8);
+    makeButton1(100,210,8);
+    drawGraphics(0);
+    
   while(digitalRead(WIO_5S_PRESS)){
     if (digitalRead(WIO_5S_UP) == LOW){
       selection=0;
@@ -98,24 +115,84 @@ int menu(){
 
 
 void drawGraphics(int s){
-  tft.drawRect(x,y,280,40,green);
-  x=20;
-  y=10 + 40*s;
-  tft.drawRect(x,y,280,40,31);
-  for(int i=0;i<2;i++){
-  tft.setCursor((320 - tft.textWidth(menuNames[i]))/2, 20+i*40);
-  tft.print(menuNames[i]);
-   }
+    tft.drawRect(x,y,280,40,TFT_GREEN);
+    x=20;
+    y=10 + 40*s;
+    tft.drawRect(x,y,280,40,31);
+    for(int i=0;i<2;i++){
+    tft.setTextSize(2);
+    tft.setCursor((320 - tft.textWidth(menuNames[i]))/2, 20+i*40);
+    tft.print(menuNames[i]);
+     }
 }
 
+void makeButton(int x,int y,int r){
+    tft.drawCircle(x,y,r*4+10 ,TFT_BLUE);
+    tft.drawCircle(x,y,r, TFT_BLUE);
+    tft.fillTriangle(x,y-r*4,x-r*2,y-r*2,x+r*2,y-r*2, TFT_BLUE);
+    tft.fillTriangle(x,y+r*4,x-r*2,y+r*2,x+r*2,y+r*2, TFT_BLUE);
+    tft.fillCircle(x,y,r-r/3,TFT_BLUE);
+    tft.setTextSize(2);
+    tft.drawString("Select", x+r*8,y-r/2);
+}
 
+void makeButton1(int x, int y, int r){
+    tft.fillCircle(x,y,r,TFT_BLUE);
+    tft.drawCircle(x,y,r+r/3, TFT_BLUE);
+    tft.drawString("Enter", x+ r*8,y-r/2 );
+
+}
+
+void wiodisplay(){
+  tft.fillScreen(green);
+  if(state=="run"){
+    tft.setTextSize(1);
+    tft.fillRect(65,-10,50,25,6000);
+    tft.fillRect(150,-10,50,25,6000);
+    tft.setTextColor(TFT_BLACK,TFT_BLACK);
+    tft.drawString("GRAPH",70,3);
+    tft.drawString("EXIT",160,3);
+    tft.setTextSize(3);
+    
+    tft.drawString(" Running Mode", 30,50 );
+    tft.drawCircle(160,200,50, TFT_RED);
+    tft.fillRect(110,200,100,100, TFT_GREEN);
+  }
+  else if(state=="train"){
+    tft.setTextSize(1);
+    tft.fillRect(65,-10,50,25,6000);
+    tft.fillRect(150,-10,50,25,6000);
+    tft.setTextColor(TFT_BLACK,TFT_BLACK);
+    tft.drawString("DELETE",70,3);
+    tft.drawString("EXIT",160,3);
+    tft.setTextSize(3);
+    
+    tft.drawString(" Training Mode", 30,50 );
+    tft.drawString("________________________",3,15);
+    tft.drawRoundRect(80-5,110-5,165,80,20, TFT_WHITE);
+    tft.fillRoundRect(80,110,165,80,20, TFT_GREEN);
+    displayTraining();
+    tft.setTextSize(2);
+
+  }
+}
 void info(){
   tft.fillScreen(green);
+    tft.setTextSize(1);
+    tft.fillRect(65,-10,50,25,6000);
+    tft.fillRect(150,-10,50,25,6000);
+    tft.setTextColor(TFT_BLACK,TFT_BLACK);
+
+    tft.drawString("EXIT",160,3);
+        tft.setTextSize(3);
   while(digitalRead(WIO_KEY_A)){
-    tft.drawString("not for a long time",50,50);
-}
+    tft.drawString("Coming soon... ",50,50);
+  }
  tft.fillScreen(green);
+ selection=0;
 }
+
+
 void buzz(int t){
   for (int i=0;i <t; i++){
       analogWrite(WIO_BUZZER,128);
@@ -124,15 +201,39 @@ void buzz(int t){
       delay(200);
   }
 }
-
+void displayTraining(){
+    tft.setTextSize(5);
+    tft.fillRoundRect(80,110,165,80,20, TFT_GREEN);
+    itoa(DCount,howMany,10) ;
+    strcat(howMany,"/10");
+    tft.drawString(howMany,(320 - tft.textWidth(howMany))/2,125); 
+    tft.setTextSize(2);
+}
 
 void reset_wio(){ //resets all the training data to 0 
       DCount=0;
-      state="Training";
+      state="train";
       state_flag=true;
       myservo1.write(0);
       pos=0;
       i=0;
+      tft.fillScreen(green);
+}
+
+
+
+
+int showmotor(int pos){
+  
+  tft.fillCircle(x_motor, y_motor , 10, TFT_BLUE);
+  x_motor=160-60*cos((180-pos)*PI/180);
+  y_motor=200-60*sin((180-pos)*PI/180);
+  tft.fillCircle(x_motor, y_motor , 10, TFT_WHITE);
+  
+  
+
+  
+  
 }
 
 int smartMotor(){
@@ -176,6 +277,12 @@ int smartMotor(){
       count=0;
       inc=1;
     }
+    else if(digitalRead(WIO_KEY_B)==LOW){
+     while(digitalRead(WIO_KEY_B)==LOW);
+     DCount=DCount-1;
+     displayTraining();
+      
+    }
     if(digitalRead(WIO_5S_PRESS) == LOW){
       if(DCount<10){
         buzz(1);
@@ -183,11 +290,12 @@ int smartMotor(){
         training[i][1]=brightness;
         i=i+1;
         DCount+=1;
+        displayTraining();
        }
        else{
-        state="Running";
+        state="run";
         state_flag=true;
-       // wio_display();
+        wiodisplay();
         buzz(3);
   
         while(digitalRead(WIO_KEY_A)){  //pressing the button will escape the run mode
@@ -198,15 +306,18 @@ int smartMotor(){
             dist=abs(brightness - training[i][1]);
             if (dist<mini){
               mini=dist;
-              pos=training[i][0];
-              
+              pos=training[i][0];  
             }
           }
          myservo1.write(pos);
-         delay(100);
+         if (pos!=old_pos){
+          showmotor(pos);
+          old_pos=pos;
+          }
+          delay(100);
         }
-        reset_wio();
        }
       }
   }
+ reset_wio();
 }
